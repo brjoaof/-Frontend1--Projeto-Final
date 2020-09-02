80c3868c732e97c3ac447c5927c0ef19deccaa71
brjoaof <brjoaof@gmail.com>
2020-09-02 03:49:45 -0300

    Add Login Mocado e Persistencia com sessionStorage

--- js/script.js
+++ js/script.js
@@ -1,3 +1,37 @@
+if(sessionStorage.getItem('email') != undefined) {
+        document.querySelector("#dropdownMenuButton").innerHTML = "Olá, João Felipe!!!<br>Seja Bem-vindo!"
+        $('#menuLogin').hide();
+        $('#menuCliente').show();
+}
+    
+
+function verLogin() {
+    console.log("oi")
+    $('#dropdownMenuButton').dropdown('toggle')
+}
+
 function login() {
-    $("#formLogin").modal();
+    var email = document.querySelector("#email").value
+    var senha = document.querySelector("#senha").value
+  
+    if(email == "joao@tmj.com.br" && senha == "tmj123"){
+         sessionStorage.setItem('email', email);
+         sessionStorage.setItem('senha', senha);
+        alert("Seja Bem-vindo, João!")
+        verLogin();
+        document.querySelector("#dropdownMenuButton").innerHTML = "Olá, João Felipe!!!<br>Seja Bem-vindo!"
+        $('#menuLogin').hide();
+        $('#menuCliente').show();
+    }
+    else
+        alert("ERRO: Verifique seu Login e Senha")
+
+    
+}
+
+function sair() {
+    sessionStorage.removeItem('email');
+    sessionStorage.removeItem('senha');
+    alert("Obrigado pela visita. Volte Sempre!");
+    location.reload();
 }
